320e5a76435ba76e0b9f763b5b298c890046c167
Patrick <Patrick.Dijkwel@student.hu.nl>
2016-11-08 13:42:16 +0100

    Test2

--- Test.py
+++ Test.py
@@ -1,5 +1,23 @@
-tekst = 'wat=wat'
-for i in tekst:
-    regelsplit = tekst.split('=')
-    regelsplit[1] = 'Graftak'
-    print(regelsplit+regelsplit[1])
+import requests
+import xmltodict
+
+auth_details = ('Patrick.Dijkwel@student.hu.nl', '1zC8XifCQz0754NWzPeUumzDBpCRikAl9pMbJnymq815s710wN-YVA')
+
+invoer = input("Wat is uw locatie? ")
+api_url = 'http://webservices.ns.nl/ns-api-avt?station=' + invoer
+
+response = requests.get(api_url, auth=auth_details)
+
+vertrekXML = xmltodict.parse(response.text)
+
+print('Dit zijn de vertrekkende treinen')
+for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
+    eindbestemming=vertrek['EindBestemming']
+    vertrektijd = vertrek['VertrekTijd']
+    vertrektijd = vertrektijd[-13:-8]
+    try:
+        vertraging = vertrek['VertrekVertragingTekst']
+        if vertraging is not None:
+            print('Om '+vertrektijd+' vertrekt een trein naar '+ eindbestemming + ' met een vertraging van ' + vertraging)
+    except KeyError:
+        print('Om '+vertrektijd+' vertrekt een trein naar '+ eindbestemming)
