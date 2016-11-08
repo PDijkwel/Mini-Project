e50db5227312221bf2404829196223a40df7aa94
Patrick <Patrick.Dijkwel@student.hu.nl>
2016-11-08 15:24:27 +0100

    Systeem

--- Test.py
+++ Test.py
@@ -1,25 +1,53 @@
-import requests
-import xmltodict
+def vertrektijdenUtrecht():
+    import requests
+    import xmltodict
 
-auth_details = ('Patrick.Dijkwel@student.hu.nl', '1zC8XifCQz0754NWzPeUumzDBpCRikAl9pMbJnymq815s710wN-YVA')
+    auth_details = ('Patrick.Dijkwel@student.hu.nl', '1zC8XifCQz0754NWzPeUumzDBpCRikAl9pMbJnymq815s710wN-YVA')
+    api_url = 'http://webservices.ns.nl/ns-api-avt?station=Ut'
 
-invoer = input("Wat is uw locatie? ")
-api_url = 'http://webservices.ns.nl/ns-api-avt?station=' + invoer
+    response = requests.get(api_url, auth=auth_details)
 
-response = requests.get(api_url, auth=auth_details)
+    vertrekXML = xmltodict.parse(response.text)
 
-vertrekXML = xmltodict.parse(response.text)
+    print('Dit zijn de vertrekkende treinen')
+    for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
+        eindbestemming=vertrek['EindBestemming']
+        vertrekspoor = vertrek['VertrekSpoor']['#text']
+        vertrektijd = vertrek['VertrekTijd']
+        vertrektijd = vertrektijd[-13:-8]
+        try:
+            vertraging = vertrek['VertrekVertragingTekst']
+            if vertraging is not None:
+                print('Om '+vertrektijd+' vertrekt een trein naar '+ eindbestemming + ' vanaf spoor ' + vertrekspoor +' met een vertraging van ' + vertraging)
+        except KeyError:
+            print('Om '+vertrektijd+' vertrekt een trein naar ' + eindbestemming + ' vanaf spoor ' + vertrekspoor)
 
-print('Dit zijn de vertrekkende treinen')
-for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
-    eindbestemming=vertrek['EindBestemming']
-    vertrektijd = vertrek['VertrekTijd']
-    vertrektijd = vertrektijd[-13:-8]
-    try:
-        vertraging = vertrek['VertrekVertragingTekst']
-        if vertraging is not None:
-            print('Om '+vertrektijd+' vertrekt een trein naar '+ eindbestemming + ' met een vertraging van ' + vertraging)
-    except KeyError:
-        print('Om '+vertrektijd+' vertrekt een trein naar '+ eindbestemming)
+def vertrektijdenOpvragen():
+    import requests
+    import xmltodict
 
+    auth_details = ('Patrick.Dijkwel@student.hu.nl', '1zC8XifCQz0754NWzPeUumzDBpCRikAl9pMbJnymq815s710wN-YVA')
 
+    while True:
+        invoer = input("Wat is uw locatie? ")
+        api_url = 'http://webservices.ns.nl/ns-api-avt?station=' + invoer
+
+        response = requests.get(api_url, auth=auth_details)
+
+        vertrekXML = xmltodict.parse(response.text)
+
+        print('Dit zijn de vertrekkende treinen')
+        for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
+            eindbestemming=vertrek['EindBestemming']
+            vertrekspoor = vertrek['VertrekSpoor']['#text']
+            vertrektijd = vertrek['VertrekTijd']
+            vertrektijd = vertrektijd[-13:-8]
+            try:
+                vertraging = vertrek['VertrekVertragingTekst']
+                if vertraging is not None:
+                    print('Om {} vertrekt een trein naar {:22} vanaf spoor {}\n                                              '
+                          '          met een vertraging van {}\n'.format(vertrektijd, eindbestemming, vertrekspoor, vertraging))
+            except KeyError:
+                print('Om {} vertrekt een trein naar {:22} vanaf spoor {}\n'.format(vertrektijd, eindbestemming, vertrekspoor))
+
+vertrektijdenOpvragen()
