1389ce679849ec0689862bf92d09500d54e158b6
Patrick <Patrick.Dijkwel@student.hu.nl>
2016-11-10 18:28:10 +0100

    Systeem

--- Test.py
+++ Test.py
@@ -1,3 +1,37 @@
+def stationsnamen(stat):
+    import requests
+    import xmltodict
+
+    auth_details = ('Patrick.Dijkwel@student.hu.nl', '1zC8XifCQz0754NWzPeUumzDBpCRikAl9pMbJnymq815s710wN-YVA')
+    api_url = 'http://webservices.ns.nl/ns-api-stations-v2'
+    response1 = requests.get(api_url, auth=auth_details)
+    stationXML = xmltodict.parse(response1.text)
+    langeNaam = ""
+    lijstStations = []
+    lijstCodeNamen = []
+    for station in stationXML['Stations']['Station']:
+        stationNaamKort = station['Namen']['Kort']
+        stationNaamMiddel = station['Namen']['Middel']
+        stationNaamLang = station['Namen']['Lang']
+        codeNaam = station['Code']
+
+        if stat in codeNaam:
+            langeNaam = station['Namen']['Lang']
+            lijstCodeNamen.append(codeNaam)
+        elif stat in stationNaamKort:
+            lijstStations.append(stationNaamKort)
+        elif stat in stationNaamMiddel:
+            lijstStations.append(stationNaamMiddel)
+        elif stat in stationNaamLang:
+            lijstStations.append(stationNaamLang)
+
+    if stat in lijstCodeNamen:
+        return('Dit zijn de vertrekkende treinen van station ' + stat + "(" + langeNaam + ")" '\n' + vertrektijdenOpvragen(stat))
+    elif stat in lijstStations:
+        return('Dit zijn de vertrekkende treinen van station ' + stat + '\n' + vertrektijdenOpvragen(stat))
+    else:
+        return("Het ingevoerde station bestaat niet")
+
 def vertrektijdenUtrecht():
     import requests
     import xmltodict
@@ -19,9 +53,9 @@ def vertrektijdenUtrecht():
         try:
             vertraging = vertrek['VertrekVertragingTekst']
             if vertraging is not None:
-                Tijden += ('Om {} vertrekt een trein naar {:40} vanaf spoor {}\n met een vertraging van {}\n'.format(vertrektijd, eindbestemming, vertrekspoor, vertraging))
+                Tijden += ('Om {} vertrekt een trein naar {:10} vanaf spoor {}\n met een vertraging van {}\n'.format(vertrektijd, eindbestemming, vertrekspoor, vertraging))
         except KeyError:
-            Tijden += ('Om {} vertrekt een trein naar {:40} vanaf spoor {}\n'.format(vertrektijd, eindbestemming, vertrekspoor))
+            Tijden += ('Om {} vertrekt een trein naar {:10} vanaf spoor {}\n'.format(vertrektijd, eindbestemming, vertrekspoor))
     return Tijden
 
 def vertrektijdenOpvragen(invoer):
@@ -38,7 +72,6 @@ def vertrektijdenOpvragen(invoer):
 
     vertrekXML = xmltodict.parse(response.text)
 
-    Tijden += ('Dit zijn de vertrekkende treinen\n')
     for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
         eindbestemming=vertrek['EindBestemming']
         vertrekspoor = vertrek['VertrekSpoor']['#text']
@@ -47,8 +80,7 @@ def vertrektijdenOpvragen(invoer):
         try:
             vertraging = vertrek['VertrekVertragingTekst']
             if vertraging is not None:
-                Tijden += ('Om {} vertrekt een trein naar {:22} vanaf spoor {}\n                                              '
-                      '          met een vertraging van {}\n'.format(vertrektijd, eindbestemming, vertrekspoor, vertraging))
+                Tijden += ('Om {} vertrekt een trein naar {:22} vanaf spoor {}\nmet een vertraging van {}\n\n'.format(vertrektijd, eindbestemming, vertrekspoor, vertraging))
         except KeyError:
             Tijden += ('Om {} vertrekt een trein naar {:22} vanaf spoor {}\n'.format(vertrektijd, eindbestemming, vertrekspoor))
     return Tijden
