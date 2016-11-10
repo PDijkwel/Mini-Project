338d8e2d3e45cf7c23410a7d0cb58fa8644f3481
Patrick <Patrick.Dijkwel@student.hu.nl>
2016-11-10 15:36:41 +0100

    Systeem

--- Test.py
+++ Test.py
@@ -8,8 +8,37 @@ def vertrektijdenUtrecht():
     response = requests.get(api_url, auth=auth_details)
 
     vertrekXML = xmltodict.parse(response.text)
+    Tijden = ""
+    Tijden += ('Dit zijn de vertrekkende treinen\n')
+    for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
+        eindbestemming=vertrek['EindBestemming']
+        vertrektijd = vertrek['VertrekTijd']
+        vertrekspoor = vertrek['VertrekSpoor']['#text']
+        vertrektijd = vertrektijd[-13:-8]
+        treinsoort = vertrek['TreinSoort']
+        try:
+            vertraging = vertrek['VertrekVertragingTekst']
+            if vertraging is not None:
+                Tijden += ('Om {} vertrekt een trein naar {:40} vanaf spoor {}\n met een vertraging van {}\n'.format(vertrektijd, eindbestemming, vertrekspoor, vertraging))
+        except KeyError:
+            Tijden += ('Om {} vertrekt een trein naar {:40} vanaf spoor {}\n'.format(vertrektijd, eindbestemming, vertrekspoor))
+    return Tijden
 
-    print('Dit zijn de vertrekkende treinen')
+def vertrektijdenOpvragen(invoer):
+    import requests
+    import xmltodict
+    Tijden = ""
+
+    auth_details = ('Patrick.Dijkwel@student.hu.nl', '1zC8XifCQz0754NWzPeUumzDBpCRikAl9pMbJnymq815s710wN-YVA')
+
+
+    api_url = 'http://webservices.ns.nl/ns-api-avt?station=' + invoer
+
+    response = requests.get(api_url, auth=auth_details)
+
+    vertrekXML = xmltodict.parse(response.text)
+
+    Tijden += ('Dit zijn de vertrekkende treinen\n')
     for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
         eindbestemming=vertrek['EindBestemming']
         vertrekspoor = vertrek['VertrekSpoor']['#text']
@@ -18,35 +47,9 @@ def vertrektijdenUtrecht():
         try:
             vertraging = vertrek['VertrekVertragingTekst']
             if vertraging is not None:
-                print('Om {} vertrekt een trein naar {:22} vanaf spoor {}\n                                              '
-                          '          met een vertraging van {}\n'.format(vertrektijd, eindbestemming, vertrekspoor, vertraging))
+                Tijden += ('Om {} vertrekt een trein naar {:22} vanaf spoor {}\n                                              '
+                      '          met een vertraging van {}\n'.format(vertrektijd, eindbestemming, vertrekspoor, vertraging))
         except KeyError:
-            print('Om {} vertrekt een trein naar {:22} vanaf spoor {}\n'.format(vertrektijd, eindbestemming, vertrekspoor))
+            Tijden += ('Om {} vertrekt een trein naar {:22} vanaf spoor {}\n'.format(vertrektijd, eindbestemming, vertrekspoor))
+    return Tijden
 
-def vertrektijdenOpvragen():
-    import requests
-    import xmltodict
-
-    auth_details = ('Patrick.Dijkwel@student.hu.nl', '1zC8XifCQz0754NWzPeUumzDBpCRikAl9pMbJnymq815s710wN-YVA')
-
-    while True:
-        invoer = input("Wat is uw locatie? ")
-        api_url = 'http://webservices.ns.nl/ns-api-avt?station=' + invoer
-
-        response = requests.get(api_url, auth=auth_details)
-
-        vertrekXML = xmltodict.parse(response.text)
-
-        print('Dit zijn de vertrekkende treinen')
-        for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
-            eindbestemming=vertrek['EindBestemming']
-            vertrekspoor = vertrek['VertrekSpoor']['#text']
-            vertrektijd = vertrek['VertrekTijd']
-            vertrektijd = vertrektijd[-13:-8]
-            try:
-                vertraging = vertrek['VertrekVertragingTekst']
-                if vertraging is not None:
-                    print('Om {} vertrekt een trein naar {:22} vanaf spoor {}\n                                              '
-                          '          met een vertraging van {}\n'.format(vertrektijd, eindbestemming, vertrekspoor, vertraging))
-            except KeyError:
-                print('Om {} vertrekt een trein naar {:22} vanaf spoor {}\n'.format(vertrektijd, eindbestemming, vertrekspoor))
